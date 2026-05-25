403eddd83ce075f6a566dda780334bef9b2979fb
chanakya <you@example.com>
2026-05-25 16:56:27 +0530

    terraform

--- for_each/variables.tf
+++ for_each/variables.tf
@@ -1,14 +1,19 @@
  # Map
-variable "instances" {
-     type = map
-     default = {
-         mongodb = "t3.micro"
-         mysql = "t3.small"
-         user = "t3.micro"
-         shipping = "t3.small"
-     }
- }
+# variable "instances" {
+#      type = map
+#      default = {
+#          mongodb = "t3.micro"
+#          mysql = "t3.small"
+#          user = "t3.micro"
+#          shipping = "t3.small"
+#      }
+#  }
 
+This should be converted into set
+
+variable "instances" {
+    type = list
+    default = ["mongodb", "redis"]
 
 # This should be converted into set
 # variable "instances" {
